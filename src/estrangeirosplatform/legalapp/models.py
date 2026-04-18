aef27c51a522fc99d54fe1f2fe33cc8258dcef04
Pedro Pires <pires.phgp@gmail.com>
2026-04-18 02:20:43 -0300

    web: shift_valor_acordo percent form the recommendation

--- src/estrangeirosplatform/legalapp/models.py
+++ src/estrangeirosplatform/legalapp/models.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.validators import RegexValidator
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
@@ -198,6 +201,42 @@ class LawyerAction(TimeStampedModel):
 
 	class Meta:
 		ordering = ['-created_at']
+
+	def _calculate_recommendation_alignment(self):
+		try:
+			recommendation = self.case.recommendation
+		except ObjectDoesNotExist:
+			recommendation = None
+
+		if recommendation is None:
+			self.same_action_taken = False
+			self.valor_acordo_in_range = None
+			self.shift_valor_acordo = None
+			return
+
+		self.same_action_taken = (self.acao == recommendation.sugestao_acao)
+
+		if (
+			self.acao == 'PROPOR_ACORDO'
+			and recommendation.sugestao_acao == 'PROPOR_ACORDO'
+			and self.valor_acordo is not None
+			and recommendation.valor_para_acordo is not None
+		):
+			valor_recomendado = recommendation.valor_para_acordo
+			limite_inferior = valor_recomendado * Decimal('0.80')
+			limite_superior = valor_recomendado * Decimal('1.20')
+			self.valor_acordo_in_range = limite_inferior <= self.valor_acordo <= limite_superior
+			if valor_recomendado != 0:
+				self.shift_valor_acordo = ((self.valor_acordo - valor_recomendado) / valor_recomendado) * Decimal('100')
+			else:
+				self.shift_valor_acordo = None
+		else:
+			self.valor_acordo_in_range = None
+			self.shift_valor_acordo = None
+
+	def save(self, *args, **kwargs):
+		self._calculate_recommendation_alignment()
+		return super().save(*args, **kwargs)
 
 	def __str__(self):
 		return f'{self.case.numero_processo} - {self.acao}'
